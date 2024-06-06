b9b5ff0fc90473ca30847212e901209f73632a44
Cantarella <zdanevich@progressivemedia.ru>
2024-06-06 14:24:50 +0300

    настрйока конфига деплоя

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/Cantarella/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy': `scp -Cr ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_ENVIRONMENTS}`,
+      'pre-deploy': `scp -C ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH_ENVIRONMENTS}`,
       'post-deploy': 'npm i && npm run start',
     },
   },
