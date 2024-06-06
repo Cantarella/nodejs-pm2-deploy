88bfb593da782156e573b4064ba4d117f8eb0a77
Cantarella <zdanevich@progressivemedia.ru>
2024-06-06 23:20:34 +0300

    добавлена конфигурация для фронтенд

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+require('@dotenvx/dotenvx').config({
+  path: `${__dirname}/.env.deploy`
+})
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_PATH_ENVIRONMENTS, DEPLOY_REF = 'origin/master',
+} = process.env;
+console.log(DEPLOY_HOST, DEPLOY_PATH, DEPLOY_USER);
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/Cantarella/web-plus-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp -Cv ./.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH} && scp -C ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `whoami && cd web-plus-pm2-deploy/source/backend && npm i && npm run start`,
+    },
+  },
+};
