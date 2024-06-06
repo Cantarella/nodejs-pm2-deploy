51335b9937fd608418c2a8e866fca3082b781c2c
Cantarella <zdanevich@progressivemedia.ru>
2024-06-06 23:29:28 +0300

    изменен конфиг фронта

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/Cantarella/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'post-deploy': `whoami && cd ~/web-plus-pm2-deploy/source/frontend && npm i && npm run start`,
+      'post-deploy': `cd ~/web-plus-pm2-deploy/source/frontend && whoami && npm i && npm run start`,
     },
   },
 };
