e57e0d9ee5293a36d3be316efe55bdbdee4c828a
Cantarella <zdanevich@progressivemedia.ru>
2024-06-06 23:11:22 +0300

    настройка конфига, очередная

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
   deploy: {
     production: {
       user: DEPLOY_USER,
-      key: '~/.ssh/id_ed25519.pub',
+      key: 'ecdsa-sha2-nistp256 SHA256:cpk6kPQ+SK/5XBN8d1yKU+ciPIJmnRm1O61T52s7Aq8',
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
       repo: 'https://github.com/Cantarella/web-plus-pm2-deploy.git',
