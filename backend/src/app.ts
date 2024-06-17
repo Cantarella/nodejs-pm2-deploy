346f56038dc898f5141b516ee5f84313891c780a
Cantarella <zdanevich@progressivemedia.ru>
2024-06-17 18:53:39 +0300

    добавила краш-тест

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,7 @@ import { errors } from 'celebrate';
 import cors from 'cors';
 import errorHandler from './middlewares/error-handler';
 import { DB_ADDRESS } from './config';
+import router from "./routes";
 import routes from './routes';
 
 const { PORT = 3000 } = process.env;
@@ -28,6 +29,11 @@ app.use(cookieParser());
 app.use(routes);
 app.use(errors());
 app.use(errorHandler);
+app.use('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log('ok'));
